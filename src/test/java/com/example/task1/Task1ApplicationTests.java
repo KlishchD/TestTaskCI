40988c9c4fd1ffde845b11e8a10ae3ceb8af97ec
Dmytro Klishch <dklishch@n-ix.com>
2024-10-01 18:12:36 +0300

    Added test

--- src/test/java/com/example/task1/Task1ApplicationTests.java
+++ src/test/java/com/example/task1/Task1ApplicationTests.java
@@ -2,12 +2,19 @@ package com.example.task1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Task1ApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void failTest()
+    {
+        assertThat(false).isTrue();
     }
 
 }
